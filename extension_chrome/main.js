589e4355d8fc485836957264ff879f73812e8f0a
Carl Schwope <schwope.carl@gmail.com>
2013-10-17 17:16:05 -0400

    working thing

--- extension_chrome/main.js
+++ extension_chrome/main.js
@@ -1,12 +1,42 @@
+/*
 function generateSearchResults( html, url ){
+  alert('this');
 
   // if url is local host
-  query = processLocalHost( html );
+//  query = processLocalHost( html );
 
   // use the query at stack overflow
-  result = queryStackOverflow( query );
+ // result = queryStackOverflow( query );
 
   // compile the result into an html string ( or whatever we need it to be )
-  new_html = compileResult( result );
-  return new_html;
-};
+ // new_html = compileResult( result );
+  return 'string';
+}
+
+
+function myRequest( request, sender, sendResponse ){
+
+  alert( 'hey, that happened' );
+
+}
+
+
+chrome.extension.onRequest.addListener( myRequest );
+*/
+
+chrome.runtime.onMessage.addListener(
+  function( request, sender, sendResponse){
+    alert('here');
+  }
+);
+
+
+
+
+
+
+
+
+
+
+
